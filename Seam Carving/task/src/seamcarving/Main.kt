522cad1ae47d6353d5de9e2aa3d3cfcae96a7525
Max Zhilin <max_zhilin@mail.ru>
2022-10-13 23:49:31 +0300

    Stage 4 Seam

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -10,51 +10,46 @@ fun main(args: Array<String>) {
     val inName = args[1]
     val outName = args[3]
     val image = ImageIO.read(File(inName))
-    val energy: Array<Array<Double>> = Array(image.width ) { Array(image.height) { 0.0 } }
-    for (x in 0 until image.width)
-        for (y in 0 until image.height) {
-            val xd = when (x) {
-                0 -> 1
-                image.width - 1 -> x - 1
-                else -> x
-            }
-            val yd = when (y) {
-                0 -> 1
-                image.height - 1 -> y - 1
-                else -> y
-            }
+    val lastX = image.width - 1
+    val lastY = image.height - 1
+    val minEnergySum: Array<Array<Double>> = Array(image.width) { Array(image.height) { 0.0 } }
+    for (y in 0..lastY) // Row by row fill top down
+        for (x in 0..lastX) {
+            val xd = x.coerceIn(1 until lastX) // Shift by 1 near the borders
+            val yd = y.coerceIn(1 until lastY)
 
             val colorX1 = Color(image.getRGB(xd - 1, y))
             val colorX2 = Color(image.getRGB(xd + 1, y))
             val colorY1 = Color(image.getRGB(x, yd - 1))
             val colorY2 = Color(image.getRGB(x, yd + 1))
 
-            energy[x][y] = sqrt(deltaSquare(colorX1, colorX2) + deltaSquare(colorY1, colorY2))
+            // Don't need to store energy due to one pass
+            val energyXY = sqrt(deltaSquare(colorX1, colorX2) + deltaSquare(colorY1, colorY2))
+
+            // *** Part Two ***
+            minEnergySum[x][y] = energyXY +
+                    if (y > 0) {
+                        val indices = when (x) { // Use 3 pixels one line above
+                            0 -> 0..1       // .. sometimes 2
+                            lastX -> x - 1..x
+                            else -> x - 1..x + 1
+                        }
+                        indices.minOf { minEnergySum[it][y - 1] }
+                    } else 0.0 // For first line it's just energy
         }
-    val maxEnergyValue = energy.maxOf { innerArray -> innerArray.maxOf { it } }
-    for (x in 0 until image.width)
-        for (y in 0 until image.height) {
-            val intensity = (255.0 * energy[x][y] / maxEnergyValue).toInt()
-            val newRGB = Color(intensity, intensity, intensity).rgb
-            image.setRGB(x, y, newRGB)
+
+    // Take min sum on the bottom line and reconstruct the shortest path line by line bottom up
+    var x = minEnergySum.indices.minByOrNull { minEnergySum[it][lastY] }!!
+    image.setRGB(x, lastY, Color.RED.rgb)
+    for (y in lastY - 1 downTo 0) {
+        val indices = when (x) {
+            0 -> 0..1
+            lastX -> x - 1..x
+            else -> x - 1..x + 1
         }
-
-    // *** Part Two ***
-    val minEnergySum: Array<Array<Double>> = Array(image.width ) { Array(image.height) { 0.0 } }
-
-    for (x in 0 until image.width)
-        for (y in 0 until image.height)
-            minEnergySum[x][y] = energy[x][y] +
-                    if (y > 0)
-                        minOf(
-                            minEnergySum[x][y - 1],
-                            if (x > 0) minEnergySum[x - 1][y - 1] else 0.0,
-                            if (x < image.width - 1) minEnergySum[x + 1][y - 1] else 0.0
-                        )
-                    else 0.0
-    val minIndexAtBottom = minEnergySum.indices.minByOrNull { minEnergySum[it][image.height - 1] }
-    println(minIndexAtBottom)
-
+        x = indices.minByOrNull { minEnergySum[it][y] }!! // X where min sum in 3 (or 2) pixels on the prev line
+        image.setRGB(x, y, Color.RED.rgb)
+    }
 
     ImageIO.write(image, "png", File(outName))
 }
