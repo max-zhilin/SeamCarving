c8b64b59cc80dbd6f2f8a342c20d8c0207053820
Max Zhilin <max_zhilin@mail.ru>
2022-10-15 17:32:55 +0300

    Stage 5 Horizontal Seam

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -1,6 +1,7 @@
 package seamcarving
 
 import java.awt.Color
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 import kotlin.math.pow
@@ -9,19 +10,22 @@ import kotlin.math.sqrt
 fun main(args: Array<String>) {
     val inName = args[1]
     val outName = args[3]
+    val horizontal = true
     val image = ImageIO.read(File(inName))
-    val lastX = image.width - 1
-    val lastY = image.height - 1
-    val minEnergySum: Array<Array<Double>> = Array(image.width) { Array(image.height) { 0.0 } }
+    val (lastX, lastY) = if (horizontal) Pair(image.height - 1, image.width - 1)
+                                    else Pair(image.width - 1, image.height - 1)
+
+    val minEnergySum: Array<Array<Double>> = if (horizontal) Array(image.height) { Array(image.width) { 0.0 } }
+                                                        else Array(image.width) { Array(image.height) { 0.0 } }
     for (y in 0..lastY) // Row by row fill top down
         for (x in 0..lastX) {
             val xd = x.coerceIn(1 until lastX) // Shift by 1 near the borders
             val yd = y.coerceIn(1 until lastY)
 
-            val colorX1 = Color(image.getRGB(xd - 1, y))
-            val colorX2 = Color(image.getRGB(xd + 1, y))
-            val colorY1 = Color(image.getRGB(x, yd - 1))
-            val colorY2 = Color(image.getRGB(x, yd + 1))
+            val colorX1 = Color(image.getRGBTransposed(xd - 1, y, horizontal))
+            val colorX2 = Color(image.getRGBTransposed(xd + 1, y, horizontal))
+            val colorY1 = Color(image.getRGBTransposed(x, yd - 1, horizontal))
+            val colorY2 = Color(image.getRGBTransposed(x, yd + 1, horizontal))
 
             // Don't need to store energy due to one pass
             val energyXY = sqrt(deltaSquare(colorX1, colorX2) + deltaSquare(colorY1, colorY2))
@@ -40,7 +44,7 @@ fun main(args: Array<String>) {
 
     // Take min sum on the bottom line and reconstruct the shortest path line by line bottom up
     var x = minEnergySum.indices.minByOrNull { minEnergySum[it][lastY] }!!
-    image.setRGB(x, lastY, Color.RED.rgb)
+    image.setRGBTransposed(x, lastY, Color.RED.rgb, horizontal)
     for (y in lastY - 1 downTo 0) {
         val indices = when (x) {
             0 -> 0..1
@@ -48,7 +52,7 @@ fun main(args: Array<String>) {
             else -> x - 1..x + 1
         }
         x = indices.minByOrNull { minEnergySum[it][y] }!! // X where min sum in 3 (or 2) pixels on the prev line
-        image.setRGB(x, y, Color.RED.rgb)
+        image.setRGBTransposed(x, y, Color.RED.rgb, horizontal)
     }
 
     ImageIO.write(image, "png", File(outName))
@@ -58,4 +62,18 @@ fun deltaSquare(a: Color, b: Color): Double {
     return (a.red - b.red).toDouble().pow(2.0) +
             (a.green - b.green).toDouble().pow(2.0) +
             (a.blue - b.blue).toDouble().pow(2.0)
+}
+
+fun BufferedImage.getRGBTransposed(x: Int, y: Int, transposed: Boolean): Int {
+    return if (transposed)
+        getRGB(y, x)
+    else
+        getRGB(x, y)
+}
+
+fun BufferedImage.setRGBTransposed(x: Int, y: Int, rgb: Int, transposed: Boolean) {
+    if (transposed)
+        setRGB(y, x, rgb)
+    else
+        setRGB(x, y, rgb)
 }
